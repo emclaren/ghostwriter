46d86afe7682fd69503569216668e89644d0a663
emclaren <elgin_mclaren@hotmail.com>
2019-11-02 16:44:25 -0700

    add force reload

--- src/js/script-parchment.js
+++ src/js/script-parchment.js
@@ -1,21 +1,9 @@
-
-var currentWriting;
-var started = false;
-var elapsedTime; // for keeping track of amount of time users are on the form page
-var startTime; // the time they start
-var finished = false;
-var form;
-
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-var elapsedTime=0;
-var isCounting = false
-
-let data = [
-  ["mouseX", "mouseY", "time", "is writing", "date"],
-]; // Headers for the output data
+let started = false;
+let elapsedTime; // for keeping track of amount of time users are on the form page
+let startTime; // the time they start
+let finished = false;
+let currentWriting; //indicate if the pen is making contact to the page
+let data = [ ["mouseX", "mouseY", "time", "is writing", "date"], ]; // Headers for the output data 
 
 
 function draw() {}
@@ -24,6 +12,7 @@ function keyPressed() {
     submit();
     myp5.drawingContext.clearRect(0, 0, 50,50);
     myp5.remove();
+    location.reload(true);
     }
   return false; // prevent default
 }
@@ -33,85 +22,66 @@ const s = ( sketch ) => {
   sketch.setup = () => {
     let windowWidth = window.innerWidth;
     let windowHeight = window.innerHeight;
-    var canvasOverlay = sketch.createCanvas(windowWidth, windowHeight);
-    canvasOverlay.parent('sketch-holder');
-    sketch.stroke(255,255,255);
-    sketch.background(100, 100, 255)
+    let canvasOverlay = sketch.createCanvas(windowWidth, windowHeight); //create canvas the size of current window
+    canvasOverlay.parent('sketch-holder'); //binds the canvas to a dom element
+    sketch.fill(255); //set color of counter
+    sketch.stroke(255,255,255); //set color of pen stroke
+    sketch.background(100, 100, 255); // set background color
   }
 
 
   sketch.draw = () => {
-  
-    elapsedTime = Date.now() - startTime;
-    currentWriting = false;
-  isCounting2=isCounting;
-    // Draw the line on the screen
+    elapsedTime = Date.now() - startTime; //calculates the difference between when the pen first hits the screen and now
+    currentWriting = false; //defaults that the user is not writing
+
+    // Draws the line on the screen
     if (sketch.mouseIsPressed) {
-      isCounting = true;
+      startTimer();
       sketch.line(sketch.mouseX , sketch.mouseY, sketch.pmouseX , sketch.pmouseY);
       currentWriting = true;
     }
-    
-    drawingContext.clearRect(0, 0, 50,50);
+   
+    // Draws counter on top of the screen;
    if(elapsedTime>1){
+    drawingContext.clearRect(0, 0, 50,50);
     text(elapsedTime, 10,10);
    }
-    if (isCounting2 != isCounting){
-      startTimer();
-      fill(255);
-      text("started", 10, 10);
-    }
 
-    // console.log(currentWriting);
-
-    // During the form Section, create an array out of the coordinate data
+    // Create an array out of the coordinate data
     if (started && finished == false){
-      var singleDataPoint =[sketch.mouseX,sketch.mouseY,elapsedTime, currentWriting, dateTime];
+      let singleDataPoint =[sketch.mouseX,sketch.mouseY,elapsedTime, currentWriting, startTime];
       data.push(singleDataPoint);
-      // console.log("mouseX "+ sketch.mouseX+" ; MouseY" + sketch.mouseY);
     }
   }
 }
+
 let myp5 = new p5(s);
 
-
-
-
-
+// Adapts canvas size if window size changes
 function changeWindowSize() {
   myp5.remove();
   myp5 = new p5(s);
 }
 
-
 window.onresize = changeWindowSize;
-
-
-
-
 
 // Starts a timer when user is on the form page
 function startTimer() {
   if(started == false ){
     started = true;
-    myp5.background(200, 200, 200, 0);
     startTime = Date.now();
   }
 }
-
-
-
-
 
 // Saves and downloads the data
 function submit() {
   finished = true;
   let csvContent = "data:text/csv;charset=utf-8,"
   + data.map(e => e.join(",")).join("\n");
-  var encodedUri = encodeURI(csvContent);
-  var link = document.createElement("a");
+  let encodedUri = encodeURI(csvContent);
+  let link = document.createElement("a");
   link.setAttribute("href", encodedUri);
-  link.setAttribute("download", "stubs-data.csv");
+  link.setAttribute("download", "stubs-data-"+startTime+".csv");
   document.body.appendChild(link); // Required for FF
   link.click(); // This will download the data file named "my_data.csv".
 }
